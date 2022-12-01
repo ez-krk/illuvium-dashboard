d2f2244297f5da1c3736275541228c52d0b0993a
€$¥ <dev@krk.finance>
2022-12-01 17:34:13 +0100

    suspended data context

--- src/contexts/data.context.tsx
+++ src/contexts/data.context.tsx
@@ -1,13 +1,13 @@
-import { createContext, useState } from 'react';
+// import { createContext, useState } from 'react';
 
-export const DataContext = createContext({
-  setData: () => null,
-  data: null
-});
+// export const DataContext = createContext({
+//   setData: () => null,
+//   data: null
+// });
 
-export const UserProvider = ({ children }) => {
-  const [data, setData] = useState(null);
-  const value = { data, setData };
+// export const UserProvider = ({ children }) => {
+//   const [data, setData] = useState(null);
+//   const value = { data, setData };
 
-  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
-};
+//   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+// };
